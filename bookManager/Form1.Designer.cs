c8f27cfb52f3727405e784df2bd5e50e413afa2e
gmolinas <90208075+gmolinas@users.noreply.github.com>
2022-12-14 19:04:19 -0300

    First Commit

---
+++ bookManager/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace bookManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estante = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Titulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Autor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TextBack = new System.Windows.Forms.Label();
+            this.addBook = new System.Windows.Forms.Button();
+            this.removeBook = new System.Windows.Forms.Button();
+            this.textBoxAuthor = new System.Windows.Forms.TextBox();
+            this.textBoxGenero = new System.Windows.Forms.TextBox();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(29, 257);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cargar Libro/s";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Autor,
+            this.Genero,
+            this.Titulo,
+            this.Estante,
+            this.Cantidad});
+            this.dataGridView.Location = new System.Drawing.Point(144, 27);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(533, 199);
+            this.dataGridView.TabIndex = 3;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.Frozen = true;
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            this.Cantidad.ReadOnly = true;
+            // 
+            // Estante
+            // 
+            this.Estante.Frozen = true;
+            this.Estante.HeaderText = "Estante";
+            this.Estante.Name = "Estante";
+            this.Estante.ReadOnly = true;
+            // 
+            // Titulo
+            // 
+            this.Titulo.Frozen = true;
+            this.Titulo.HeaderText = "Titulo";
+            this.Titulo.Name = "Titulo";
+            this.Titulo.ReadOnly = true;
+            // 
+            // Genero
+            // 
+            this.Genero.Frozen = true;
+            this.Genero.HeaderText = "Genero";
+            this.Genero.Name = "Genero";
+            this.Genero.ReadOnly = true;
+            // 
+            // Autor
+            // 
+            this.Autor.Frozen = true;
+            this.Autor.HeaderText = "Autor";
+            this.Autor.Name = "Autor";
+            this.Autor.ReadOnly = true;
+            // 
+            // TextBack
+            // 
+            this.TextBack.AutoSize = true;
+            this.TextBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.TextBack.Location = new System.Drawing.Point(218, 116);
+            this.TextBack.Name = "TextBack";
+            this.TextBack.Size = new System.Drawing.Size(376, 62);
+            this.TextBack.TabIndex = 4;
+            this.TextBack.Text = "Seleccione una base de datos\r\npara cargar los libros";
+            // 
+            // addBook
+            // 
+            this.addBook.Location = new System.Drawing.Point(29, 27);
+            this.addBook.Name = "addBook";
+            this.addBook.Size = new System.Drawing.Size(87, 44);
+            this.addBook.TabIndex = 5;
+            this.addBook.Text = "Añadir Libro";
+            this.addBook.UseVisualStyleBackColor = true;
+            this.addBook.Click += new System.EventHandler(this.addBook_Click);
+            // 
+            // removeBook
+            // 
+            this.removeBook.Location = new System.Drawing.Point(29, 89);
+            this.removeBook.Name = "removeBook";
+            this.removeBook.Size = new System.Drawing.Size(87, 44);
+            this.removeBook.TabIndex = 6;
+            this.removeBook.Text = "Remover Libro";
+            this.removeBook.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAuthor
+            // 
+            this.textBoxAuthor.Location = new System.Drawing.Point(144, 257);
+            this.textBoxAuthor.Name = "textBoxAuthor";
+            this.textBoxAuthor.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAuthor.TabIndex = 7;
+            // 
+            // textBoxGenero
+            // 
+            this.textBoxGenero.Location = new System.Drawing.Point(250, 257);
+            this.textBoxGenero.Name = "textBoxGenero";
+            this.textBoxGenero.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGenero.TabIndex = 8;
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(356, 257);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTitulo.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 301);
+            this.Controls.Add(this.textBoxTitulo);
+            this.Controls.Add(this.textBoxGenero);
+            this.Controls.Add(this.textBoxAuthor);
+            this.Controls.Add(this.removeBook);
+            this.Controls.Add(this.addBook);
+            this.Controls.Add(this.TextBack);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.exit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Autor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Genero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Titulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Estante;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.Label TextBack;
+        private System.Windows.Forms.Button addBook;
+        private System.Windows.Forms.Button removeBook;
+        private System.Windows.Forms.TextBox textBoxAuthor;
+        private System.Windows.Forms.TextBox textBoxGenero;
+        private System.Windows.Forms.TextBox textBoxTitulo;
+    }
+}
+
---
+++ bookManager/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bookManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView.Visible = false;
+            addBook.Visible = false;
+            removeBook.Visible = false;
+        }
+
+        string booksDir;
+        string positionDir;
+
+
+        void loadData(String[] Vector, String[] location)
+        {
+            dataGridView.Rows.Clear();
+
+            foreach (var item in Vector)
+            {
+                String[] elements = item.Split(new char[] { '\u002c' });
+                string estante = "No Asignado";
+                int cantidad = 1;
+
+                foreach (var element in location)
+                {
+                    String[] pos = element.Split(new char[] { '\u002c' });
+
+                    if (pos[0].Trim() == elements[1].Trim())
+                        estante = pos[1];
+
+                }
+
+
+                foreach (DataGridViewRow row in dataGridView.Rows)
+                {
+                    if (row.Cells[2].Value.ToString().Trim() == elements[2].Trim())
+                    {
+                        dataGridView.Rows.Remove(row);
+                        cantidad++;
+                    }
+                }
+
+                dataGridView.Rows.Add(elements[0], elements[1], elements[2], estante, cantidad);
+
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            OpenFileDialog openFilebooks = new OpenFileDialog();
+            DialogResult result = openFilebooks.ShowDialog();
+
+            if (result == DialogResult.OK) // Test result.
+            {
+
+                booksDir = openFilebooks.FileName;
+                string file2;
+                DialogResult result2;
+
+                do
+                {
+                    MessageBox.Show("A continuacion Cargue las direcciones de las estanterias");
+
+                    OpenFileDialog openFileposition = new OpenFileDialog();
+                    result2 = openFileposition.ShowDialog();
+                    positionDir = openFileposition.FileName;
+
+                }
+                while (result2 != DialogResult.OK);
+
+                try
+                {
+                    dataGridView.Visible = true;
+                    TextBack.Visible = false;
+                    addBook.Visible = true;
+                    removeBook.Visible = true;
+
+
+                    FileStream position = new FileStream(positionDir, FileMode.Open);
+                    StreamReader positionReader = new StreamReader(position);
+                    String[] location = positionReader.ReadToEnd().Split(new char[] { '\n' });
+
+
+
+                    FileStream books = new FileStream(booksDir, FileMode.Open);
+                    StreamReader booksReader = new StreamReader(books);
+                    String[] Vector = booksReader.ReadToEnd().Split(new char[] { '\u003b' });
+
+                    loadData(Vector,  location);
+
+                    books.Close();
+                    booksReader.Close();
+                    position.Close();
+                    positionReader.Close();
+
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+        private void addBook_Click(object sender, EventArgs e)
+        {
+
+            FileStream position = new FileStream(positionDir, FileMode.Open);
+            StreamReader positionReader = new StreamReader(position);
+            String[] location = positionReader.ReadToEnd().Split(new char[] { '\n' });
+
+            FileStream books = new FileStream(booksDir, FileMode.Open);
+            StreamReader booksReader = new StreamReader(books);
+            String[] Vector = booksReader.ReadToEnd().Split(new char[] { '\u003b' });
+
+
+            StreamWriter booksWriter = new StreamWriter(books);
+
+
+            if(String.IsNullOrEmpty(textBoxAuthor.Text)|| String.IsNullOrEmpty(textBoxGenero.Text) || String.IsNullOrEmpty(textBoxTitulo.Text))
+            {
+                MessageBox.Show("No se puede añadir campos vacios");
+
+            }
+            else
+            {
+                booksWriter.Write(booksReader.ReadToEnd() + $";{textBoxAuthor.Text}, {textBoxGenero.Text}, {textBoxTitulo.Text}");
+                booksWriter.Close();
+                loadData(Vector, location);
+            }
+
+
+
+            books.Close();
+            booksReader.Close();
+            position.Close();
+            positionReader.Close();
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+      
+    }
+}
